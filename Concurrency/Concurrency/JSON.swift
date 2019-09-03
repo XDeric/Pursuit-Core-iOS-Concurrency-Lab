bbd879778793b4706342cad55bb8112d64c3589b
eeeric3995@yahoo.com <ihxx2s7tbu0>
2019-09-03 15:30:28 -0400

    setup storyboard

---
+++ Concurrency/Concurrency/JSON.swift
@@ -0,0 +1,42 @@
+//
+//  JSON.swift
+//  Concurrency
+//
+//  Created by EricM on 9/3/19.
+//  Copyright © 2019 EricM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Country: Codable{
+    let name: String
+    let flag: String
+    let capital: String
+    let population: Int
+    let currencies: [currency]
+
+    static func getCountry(from data: Data) -> Country{
+        do{
+            let state = try JSONDecoder().decode(Country.self, from: data)
+            return state
+        } catch let anError { fatalError( "\(anError)" )}
+    }
+}
+
+struct currency: Codable {
+    let code: String
+}
+
+
+struct Exchange: Codable{
+    let rates: Double
+    
+    
+    static func getMoney(from data: Data) -> Exchange{
+        do{
+            let value = try JSONDecoder().decode(Exchange.self, from: data)
+            return value
+        } catch let anError { fatalError( "\(anError)" )}
+    }
+}
---
+++ Concurrency/Concurrency/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Concurrency
+//
+//  Created by EricM on 9/3/19.
+//  Copyright © 2019 EricM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var capitalLabel: UILabel!
+    @IBOutlet weak var populationLabel: UILabel!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
+
